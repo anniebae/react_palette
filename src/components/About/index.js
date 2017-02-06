bf36931d9808977524ae62fa9fe65cebee30c1c2
Annie Bae <bae.annie@gmail.com>
2017-02-06 15:08:56 -0500

    universal styling vs individual unique stylesheets

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
+import css from './styles.css'
 
 class About extends Component {
 	render() {
 		return(
-			<div>About page</div>
+			<div className="aboutWrapper">
+				<div className="fixed-left">
+					<p>a b o u t</p>
+				</div>
+			</div>
 		)
 	}
 }
